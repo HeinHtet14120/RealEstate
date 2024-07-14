a45f97a9e52850e1cd8b8ab9ade888158536b374
HeinHtet444 <heinhtet@vasthive.fo>
2024-07-14 15:31:16 +0700

    npm fix

---
+++ client/src/lib/loaders.js
@@ -0,0 +1,6 @@
+// import apiRequest from "./apiRequest.js";
+// export const singlePageLoader = async ({request, params}) => {
+//     const res = await apiRequest("/"+params.id);
+
+//     return res.data;
+// }
