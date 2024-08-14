00bd0d8ca847ea97e7de564d558245ae1b23d82d
HeinHtet14120 <heinhtet14120@gmail.com>
2024-08-14 21:36:16 +0700

    cors

--- api/index.js
+++ api/index.js
@@ -14,7 +14,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:process.env.CLIENT_URL, credentials:true}));
+// app.use(cors({origin:process.env.CLIENT_URL, credentials:true}));
+app.use(cors());
 
 app.use('/api/post', postRoute);
 app.use('/api/auth', authRoute);
